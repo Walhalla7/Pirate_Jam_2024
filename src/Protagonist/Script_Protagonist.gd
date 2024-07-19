5faffdb8d25f7a556afdc9f057040bb13e984c43
a-rat-in-a-hat <157244986+a-rat-in-a-hat@users.noreply.github.com>
2024-07-19 16:41:57 -0500

    Camera + Error Fix

--- src/Protagonist/Script_Protagonist.gd
+++ src/Protagonist/Script_Protagonist.gd
@@ -124,13 +124,13 @@ func _physics_process(delta):
 	#Basic movement -> Needs change
 	#TO-DO: apply states and actions so that the direction of the movement changes as the slug lands on different surfaces
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+	if Input.is_action_pressed("move_left"):
+		direction.x += 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z += 1
-	if Input.is_action_pressed("move_back"):
 		direction.z -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
