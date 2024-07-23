952f9c8e1675a6ad2bdbc66656cd9023617ff6fa
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-07-23 13:30:34 -0400

    base wall climbing
    - Can climb up walls right, left and back
    - working on timer to unglue and jump fixes

--- src/scripts/protagonist/protagonist_script.gd
+++ src/scripts/protagonist/protagonist_script.gd
@@ -9,6 +9,7 @@ extends CharacterBody3D
 @onready var righ_detector = $Right_Detector
 @onready var left_detector = $Left_Detector
 @onready var back_detector = $Back_Detector
+@onready var crawl_timer = $Crawl_Timer
 
 
 
@@ -24,30 +25,35 @@ enum States {
 	WALL_BACK,
 	WALL_LEFT,
 	WALL_RIGHT,
-	CEILING,
 	FALLING
 }
 
-#Actions player cam take in each state
-enum Actions {
-	MOVE_LEFT,
-	MOVE_RIGHT,
-	MOVE_FORWARD,
-	MOVE_BACK,
-	DASH,
-	JUMP,
-	IDLE
-}
+
+#called when snail been on the wall too long
+func _on_crawl_timer_timeout():
+	can_crawl = false
+
+#checking if snail is currently climbing
+func _is_timer_active():
+	if (crawl_timer.time_left > 0):
+		return true
+	else:
+		return false
+
 
 #Call this function whenever the snail changes it's state along with the state you want to change it to
 func change_State(newState:States):
 	match newState:
 		States.FLOOR:
 			if curr_State != States.FLOOR:
+				can_crawl = true
+				crawl_timer.stop()
 				curr_State = States.FLOOR
 				print("Floor")	
 		States.WALL_BACK:
 			if curr_State != States.WALL_BACK:
+				if !_is_timer_active():
+					crawl_timer.start()
 				curr_State = States.WALL_BACK
 				print("WALL_BACK")	
 		States.WALL_LEFT:
@@ -68,15 +74,12 @@ func change_State(newState:States):
 const speed = 5.0
 var target_velocity = Vector3.ZERO
 const JUMP_VELOCITY = 7
-#const LERP_VAL = .15
 var sprint_modifier = 1
-var double_jump = 0
+var can_crawl = true
 
 # Gravity
 #var gravity = ProjectSettings.get_setting("physics/3d/default_gravity") #Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravityStrength = 9.8
-var GravityDirection = Vector3.DOWN #Base direction
-
 
 #======================================== 	Initialize 	==================================
 func _ready():
@@ -84,10 +87,13 @@ func _ready():
 
 #======================================== 	Process 	==================================
 func _physics_process(delta):
+	#base direction
+	var direction = Vector3.ZERO
 	
-	if  left_detector.is_colliding():
-			change_State(States.WALL_LEFT)
-	elif  righ_detector.is_colliding():
+	#Determining States
+	if left_detector.is_colliding():
+		change_State(States.WALL_LEFT)
+	elif righ_detector.is_colliding():
 		change_State(States.WALL_RIGHT)
 	elif back_detector.is_colliding():
 		change_State(States.WALL_BACK)
@@ -96,46 +102,103 @@ func _physics_process(delta):
 	else:
 		change_State(States.FALLING)
 	
-	#base direction
-	var direction = Vector3.ZERO
-	
-	
-	# Add the gravity/falling
-	if not is_on_floor():
-		target_velocity.y = target_velocity.y - (gravityStrength * delta)
+	match curr_State:
+		States.FALLING:
+			target_velocity.y = target_velocity.y - (gravityStrength * delta)
+			if Input.is_action_pressed("move_right"):
+				direction.x -= 1
+			if Input.is_action_pressed("move_left"):
+				direction.x += 1
+			if Input.is_action_pressed("move_forward"):
+				direction.z -= 1
+			if Input.is_action_pressed("move_back"):
+				direction.z += 1
+			target_velocity.x = direction.x * speed * sprint_modifier
+			target_velocity.z = direction.z * speed * sprint_modifier
 
+		States.FLOOR:
+			if Input.is_action_pressed("move_right"):
+				direction.x -= 1
+			if Input.is_action_pressed("move_left"):
+				direction.x += 1
+			if Input.is_action_pressed("move_forward"):
+				direction.z -= 1
+			if Input.is_action_pressed("move_back"):
+				direction.z += 1
+			if Input.is_action_just_pressed("jump"):
+				target_velocity.y = JUMP_VELOCITY
+			target_velocity.x = direction.x * speed * sprint_modifier
+			target_velocity.z = direction.z * speed * sprint_modifier
+				
+		States.WALL_RIGHT:
+			if Input.is_action_pressed("move_right"):
+				direction.y += 1
+			if Input.is_action_pressed("move_left"):
+				if floor_detector.is_colliding():
+					direction.x += 1
+					target_velocity.x = direction.x * speed * sprint_modifier
+				else:
+					direction.y -= 1
+			if Input.is_action_pressed("move_forward"):
+				direction.z -= 1
+			if Input.is_action_pressed("move_back"):
+				direction.z += 1
+			if Input.is_action_just_pressed("jump"):
+				target_velocity.x = JUMP_VELOCITY
+			
+			target_velocity.y = direction.y * speed/2 * sprint_modifier - (gravityStrength*3 * delta) 
+			target_velocity.z = direction.z * speed/2 * sprint_modifier
 		
-	#Double jump
-	if Input.is_action_just_pressed("jump"):
-		if is_on_floor():
-			target_velocity.y = JUMP_VELOCITY
-			double_jump = 1
-		elif !is_on_floor() && double_jump != 0:
-			target_velocity.y += JUMP_VELOCITY
-			double_jump = 0
-	
+		States.WALL_LEFT:
+			if Input.is_action_pressed("move_right"):
+				if floor_detector.is_colliding():
+					direction.x -= 1
+					target_velocity.x = direction.x * speed * sprint_modifier
+				else:
+					direction.y -= 1
+			if Input.is_action_pressed("move_left"):
+				direction.y += 1
+			if Input.is_action_pressed("move_forward"):
+				direction.z -= 1
+			if Input.is_action_pressed("move_back"):
+				direction.z += 1
+			if Input.is_action_just_pressed("jump"):
+				target_velocity.x = -JUMP_VELOCITY
+			target_velocity.y = direction.y * speed * sprint_modifier
+			target_velocity.z = direction.z * speed * sprint_modifier
+		
+		States.WALL_BACK:
+			if Input.is_action_pressed("move_right"):
+				direction.x -= 1
+			if Input.is_action_pressed("move_left"):
+				direction.x += 1
+			if Input.is_action_pressed("move_forward"):
+				if floor_detector.is_colliding():
+					direction.z -= 1
+					target_velocity.z = direction.z * speed * sprint_modifier
+				else:
+					direction.y -= 1
+			if Input.is_action_pressed("move_back"):
+				direction.y += 1
+			if Input.is_action_just_pressed("jump"):
+				target_velocity.z = -JUMP_VELOCITY
+			target_velocity.y = direction.y * speed * sprint_modifier
+			target_velocity.x = direction.x * speed * sprint_modifier
+
+
 	#Sprint Input / calculations
 	sprint_modifier = 2 if Input.is_action_pressed("sprint") else 1
 	
-	#Calculating movemnt
-	#Basic movement -> Needs change
-	#TO-DO: apply states and actions so that the direction of the movement changes as the slug lands on different surfaces
-	if Input.is_action_pressed("move_right"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_left"):
-		direction.x += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
 		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		
 	# Ground Velocity
-	target_velocity.x = direction.x * speed * sprint_modifier
-	target_velocity.z = direction.z * speed * sprint_modifier
+	
 	
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+
+
