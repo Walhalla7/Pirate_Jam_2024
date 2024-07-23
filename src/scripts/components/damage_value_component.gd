7c577c4fef269b3ad7fd265fbcec0e1e6116a723
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-07-23 13:21:40 -0400

    Revert "Health/Damage Component (#9)"
    This reverts commit 455baeb9f827799575fec96a333e84db78bc2393, reversing
    changes made to a5a4bec7ab79da4ac5736cdc9eae910a601aee0f.

--- src/scripts/components/damage_value_component.gd
+++
@@ -1,7 +0,0 @@
-extends Node3D
-
-class_name Damage_Value_Component
-
-# Here we can adjust how much damage somthing does and give it other properties i.e. knockback or other effects
-@export var damage_value := 1.0
-#@export var damage_position := Vector2
