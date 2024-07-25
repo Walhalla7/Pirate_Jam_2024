6b9c331836cdf0e5157ee0a59e07fa3b96b7f544
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-07-25 13:19:12 -0400

    Smoothed out movement
    - cleaned up movement,
    - re-added jump
    - cleaned up code
    - Next: state machine connection

--- src/scripts/protagonist/protagonist_script.gd
+++ src/scripts/protagonist/protagonist_script.gd
@@ -4,12 +4,20 @@ extends CharacterBody3D
 @onready var camera_target = $CameraTarget
 @onready var animated_sprite_3d = $AnimatedSprite3D
 
+#======================================== 	Detectors 	==================================
+@onready var floorDetectors = $Raycasts/Floor_detectors
+var is_Grounded
+
+func _check_is_grounded():
+	for raycast in floorDetectors.get_children():
+		if raycast.is_colliding():
+			return true
+	return false
 
 #======================================== 	Variables 	==================================
 #Movement variables
 const move_speed = 5.0
 const JUMP_VELOCITY = 7
-const LERP_VAL = 0.2
 var sprint_modifier = 1
 
 # Gravity
@@ -25,12 +33,16 @@ func _on_health_component_hurt():
 	print("Player has been hurt")
 
 #======================================== 	Movement 	==================================
-#func _apply_movement():
-	
-
+#Gravity applied universally
 func _apply_gravity(delta):
-	velocity.y += gravityStrength * delta
+	velocity.y -= gravityStrength * delta
 
+#function to handle jump input
+func _input(event):
+	if event.is_action("jump") && is_Grounded:
+		velocity.y = JUMP_VELOCITY
+
+#function to calulcate directional inputs and movements 
 func _get_input():
 	#we calculate movement direction based on inputs 
 	var move_direction_x = int(Input.is_action_pressed("move_left")) - int(Input.is_action_pressed("move_right"))
@@ -40,8 +52,8 @@ func _get_input():
 	sprint_modifier = 2 if Input.is_action_pressed("sprint") else 1
 	
 	#we apply the direction to velocity 
-	velocity.x = lerp(velocity.x, move_direction_x * move_speed * sprint_modifier, LERP_VAL)
-	velocity.z = lerp(velocity.z, move_direction_z * move_speed * sprint_modifier, LERP_VAL)
+	velocity.x = lerp(velocity.x, move_direction_x * move_speed * sprint_modifier, _get_h_weight())
+	velocity.z = lerp(velocity.z, move_direction_z * move_speed * sprint_modifier, _get_h_weight())
 	
 	#we apply animations/rotate sprite
 	if move_direction_x != 0 or move_direction_z != 0:
@@ -57,6 +69,11 @@ func _get_input():
 			$Sprite3D.rotation.y = 0.5
 	else:
 		animated_sprite_3d.play("idle")
+
+#turning strength based on being on the floor
+func _get_h_weight():
+	return 0.2 if is_Grounded else 0.1
+
 #======================================== 	Initialize 	==================================
 func _ready():
 	pass
@@ -64,6 +81,9 @@ func _ready():
 #======================================== 	Process 	==================================
 func _physics_process(delta):
 	_get_input()
+	_apply_gravity(delta)
+	
+	is_Grounded = _check_is_grounded()
 	
 	# Flip Sprite
 	if velocity.x > 0:
