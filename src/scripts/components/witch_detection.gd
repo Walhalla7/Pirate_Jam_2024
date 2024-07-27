b419221cebbbf0af0aff7bfadf9e83dc8c0ca074
Kyle Jardine <kjardine00@gmail.com>
2024-07-27 19:30:34 -0400

    Detection Raycast Component

---
+++ src/scripts/components/witch_detection.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+class_name WitchDetector
+
+@export var player : CharacterBody3D
+@export var lock_x = true
+@export var lock_z = false
+
+@onready var ray_cast_3d = $RayCast3D
+@onready var timer = $Timer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	# Foll
+	ray_cast_3d.global_position.x = player.global_position.x
+	# Follow the player up and down
+	ray_cast_3d.global_position.y = player.global_position.y
+	
+	
+	if ray_cast_3d.is_colliding():
+		print(str(ray_cast_3d.get_collider()))
+	
