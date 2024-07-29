4aad8116c274fb19249881f074b4c5b61cb728a2
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-07-29 13:43:12 -0400

    Wall movement almost there
    movement feels better can climb and it rotates properly but wall movement is funky

--- src/RoomObjects/ObjectScenes/wall_crack.gd
+++ src/RoomObjects/ObjectScenes/wall_crack.gd
@@ -10,7 +10,16 @@ func _ready():
 
 func _on_area_3d_body_entered(body):
 	if body.is_in_group("Slug") and endLocation:
+		var raycasts = body.find_child("Raycasts").rotation_degrees.y
 		# Teleport the body
 		if Camera:
+			# Ensure Camera is properly managed; this line might not be needed.
 			Camera.current = !Camera.current
 		body.global_transform.origin = finalPosition
+		print(body.find_child("Raycasts").rotation_degrees.y)
+		if abs(body.rotation_degrees.y) == 180:
+			body.rotation_degrees.y = -270
+			body.find_child("Raycasts").rotation_degrees.y = 180
+		else:
+			body.rotation_degrees.y = -180
+			body.find_child("Raycasts").rotation_degrees.y = 0
