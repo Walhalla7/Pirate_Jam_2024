39b32e8a75f84717cd355d99769aa40f7090f12d
Kyle Jardine <kjardine00@gmail.com>
2024-07-29 16:34:28 -0400

    Hand changes

--- src/scripts/components/witch_detection.gd
+++ src/scripts/components/witch_detection.gd
@@ -12,7 +12,8 @@ class_name WitchDetector
 @onready var point_a = $CanvasLayer/PointA
 @onready var point_b = $CanvasLayer/PointB
 
-var caught = false
+var caught = true
+var looking_at_you = false
 var t = 0.0
 
 # TO USE: Set the player to collision mask 1 and anything they can hide behind on collision layer 6
@@ -22,6 +23,8 @@ var t = 0.0
 func _ready():
 	ray_cast_3d.global_position = player.global_position
 	ray_cast_3d.global_position.z = 5
+	hand.global_position = point_a.global_position
+	timer.wait_time = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,18 +33,18 @@ func _process(delta):
 	# Follow the player up and down
 	ray_cast_3d.global_position.y = player.global_position.y
 	
-	if ray_cast_3d.is_colliding():
-		#print(str(ray_cast_3d.get_collider()))
-		
-		if ray_cast_3d.get_collider() == player:
+	if ray_cast_3d.is_colliding(): 
+		if ray_cast_3d.get_collider() == player && looking_at_you == false:
 			timer.start()
-			
 			print("start")
-			print(str(timer.time_left))
+			looking_at_you = true
 		elif ray_cast_3d.get_collider() != player || ray_cast_3d.is_colliding() == false:
 			timer.stop()
 			print("stop")
+			looking_at_you = false
 			
+	#print(str(timer.time_left))
+	
 	if caught:
 		print("caught")
 		move_hand(delta)
