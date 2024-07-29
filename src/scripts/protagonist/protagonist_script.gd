0f4e728e5f4b3da5633e3081663b80925146073b
Walhalla7 <bartoszrdzanekwp@gmail.com>
2024-07-29 16:18:14 -0400

    Remove Sprint

--- src/scripts/protagonist/protagonist_script.gd
+++ src/scripts/protagonist/protagonist_script.gd
@@ -26,8 +26,8 @@ func _check_is_grounded():
 #Movement variables
 @export var move_speed = 5.0
 @export var JUMP_VELOCITY = 7
-var sprint_modifier = 1
-@export var max_sprint_modifier = 2
+#var sprint_modifier = 1
+#@export var max_sprint_modifier = 2
 var can_crawl = true
 @export var wall_slowdown = 2
 @export var jump_vertical_strength = 9
@@ -95,7 +95,7 @@ func _handle_move_input():
 	var move_direction_z = -int(Input.is_action_pressed("move_back")) + int(Input.is_action_pressed("move_forward"))
 
 	# Sprint Input / calculations
-	sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
+	#sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
 	
 	# Apply the rotation to the move direction
 	var rotation = rotation_degrees.y
@@ -106,8 +106,8 @@ func _handle_move_input():
 	var adjusted_move_z = move_direction_x * sin_rot + move_direction_z * cos_rot
 
 	# Apply the direction to velocity
-	velocity.x = lerp(velocity.x, adjusted_move_x * move_speed * sprint_modifier, _get_h_weight())
-	velocity.z = lerp(velocity.z, adjusted_move_z * move_speed * sprint_modifier, _get_h_weight())
+	velocity.x = lerp(velocity.x, adjusted_move_x * move_speed, _get_h_weight())
+	velocity.z = lerp(velocity.z, adjusted_move_z * move_speed, _get_h_weight())
 	
 	#we apply animations/rotate sprite
 	if move_direction_x != 0 or move_direction_z != 0:
@@ -142,7 +142,7 @@ func _handle_move_Left_input():
 	move_direction_z = -int(Input.is_action_pressed("move_back")) + int(Input.is_action_pressed("move_forward"))
 	
 	#Sprint Input / calculations
-	sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
+	#sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
 	
 	# Apply the rotation to the move direction
 	var rotation = rotation_degrees.y
@@ -155,9 +155,9 @@ func _handle_move_Left_input():
 	
 	#we apply the direction to velocity 
 	if is_Grounded:
-		velocity.x = lerp(velocity.x, adjusted_move_x * move_speed * sprint_modifier, _get_h_weight())
-	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown) * sprint_modifier, _get_h_weight())
-	velocity.z = lerp(velocity.z, adjusted_move_z  * move_speed * sprint_modifier, _get_h_weight())
+		velocity.x = lerp(velocity.x, adjusted_move_x * move_speed, _get_h_weight())
+	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown), _get_h_weight())
+	velocity.z = lerp(velocity.z, adjusted_move_z  * move_speed, _get_h_weight())
 
 #function to calulcate directional inputs and movements on right wall
 func _handle_move_Right_input():
@@ -177,7 +177,7 @@ func _handle_move_Right_input():
 	move_direction_z = -int(Input.is_action_pressed("move_back")) + int(Input.is_action_pressed("move_forward"))
 	
 	#Sprint Input / calculations
-	sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
+	#sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
 	
 	# Apply the rotation to the move direction
 	var rotation = rotation_degrees.y
@@ -189,9 +189,9 @@ func _handle_move_Right_input():
 	
 	#we apply the direction to velocity 
 	if is_Grounded:
-		velocity.x = lerp(velocity.x, adjusted_move_x * move_speed * sprint_modifier, _get_h_weight())
-	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown) * sprint_modifier, _get_h_weight())
-	velocity.z = lerp(velocity.z, adjusted_move_z * move_speed * sprint_modifier, _get_h_weight())
+		velocity.x = lerp(velocity.x, adjusted_move_x * move_speed, _get_h_weight())
+	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown), _get_h_weight())
+	velocity.z = lerp(velocity.z, adjusted_move_z * move_speed, _get_h_weight())
 
 #function to calulcate directional inputs and movements on right wall
 func _handle_move_Back_input():
@@ -211,7 +211,7 @@ func _handle_move_Back_input():
 	move_direction_x = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
 	
 	#Sprint Input / calculations
-	sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
+	#sprint_modifier = max_sprint_modifier if Input.is_action_pressed("sprint") else 1
 	
 	# Apply the rotation to the move direction
 	var rotation = rotation_degrees.y
@@ -223,9 +223,9 @@ func _handle_move_Back_input():
 	
 	#we apply the direction to velocity 
 	if is_Grounded:
-		velocity.z = lerp(velocity.z, adjusted_move_z * move_speed * sprint_modifier, _get_h_weight())
-	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown) * sprint_modifier, _get_h_weight())
-	velocity.x = lerp(velocity.x, adjusted_move_x * move_speed * sprint_modifier, _get_h_weight())
+		velocity.z = lerp(velocity.z, adjusted_move_z * move_speed, _get_h_weight())
+	velocity.y = lerp(velocity.y, move_direction_y * (move_speed-wall_slowdown), _get_h_weight())
+	velocity.x = lerp(velocity.x, adjusted_move_x * move_speed, _get_h_weight())
 
 #turning strength based on being on the floor
 func _get_h_weight():
