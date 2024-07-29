bb93cf20732cbef22a4c102eb1b04fd4e1da6d78
Kyle Jardine <kjardine00@gmail.com>
2024-07-29 17:11:27 -0400

    Witch hand works needs finalizations tweaks

--- src/scripts/components/witch_detection.gd
+++ src/scripts/components/witch_detection.gd
@@ -9,12 +9,13 @@ class_name WitchDetector
 @onready var ray_cast_3d = $RayCast3D
 @onready var timer = $Timer
 @onready var hand = $CanvasLayer/Sprite2D
-@onready var point_a = $CanvasLayer/PointA
-@onready var point_b = $CanvasLayer/PointB
+@onready var offscreen_point = $CanvasLayer/Offscreen
+@onready var onscreen_point = $CanvasLayer/Onscreen
 
-var caught = true
+var caught = false
 var looking_at_you = false
 var t = 0.0
+
 
 # TO USE: Set the player to collision mask 1 and anything they can hide behind on collision layer 6
 # Connect assign player in the inspector
@@ -23,11 +24,10 @@ var t = 0.0
 func _ready():
 	ray_cast_3d.global_position = player.global_position
 	ray_cast_3d.global_position.z = 5
-	hand.global_position = point_a.global_position
 	timer.wait_time = 5
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	# Follow the player left and right
 	ray_cast_3d.global_position.x = player.global_position.x
 	# Follow the player up and down
@@ -36,25 +36,26 @@ func _process(delta):
 	if ray_cast_3d.is_colliding(): 
 		if ray_cast_3d.get_collider() == player && looking_at_you == false:
 			timer.start()
-			print("start")
 			looking_at_you = true
 		elif ray_cast_3d.get_collider() != player || ray_cast_3d.is_colliding() == false:
 			timer.stop()
-			print("stop")
 			looking_at_you = false
-			
-	#print(str(timer.time_left))
+	elif ray_cast_3d.is_colliding() == false:
+		timer.stop()
+		looking_at_you = false
+
+	if caught == true:
+		# This changes speed 
+		t += delta * 0.2
+		
+		hand.position = hand.position.lerp(onscreen_point.position, t)
+		#hand.position = hand.position.lerp(offscreen_point.position, t)
+		#player.visible = false
+
+#func move_hand(delta):
+	#t += delta * 0.4
+	#print("move hand called")
+	#hand.position = offscreen_point.position.lerp(onscreen_point.position, t)
 	
-	if caught:
-		print("caught")
-		move_hand(delta)
-		caught = false
-
-
-func move_hand(delta):
-	t += delta * 0.4
-	hand.position = point_a.position.lerp(point_b.position, t)
-	
-
 func _on_timer_timeout():
 	caught = true
