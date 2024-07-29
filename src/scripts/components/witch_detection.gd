59da5cbd0d6179f0c39b4e0c4ef21e5d2616be11
Kyle Jardine <kjardine00@gmail.com>
2024-07-29 12:57:52 -0400

    Witch detection detects player and the player can hide behind stuff

--- src/scripts/components/witch_detection.gd
+++ src/scripts/components/witch_detection.gd
@@ -9,11 +9,14 @@ class_name WitchDetector
 @onready var ray_cast_3d = $RayCast3D
 @onready var timer = $Timer
 
+signal caught
+
 # TO USE: Set the player to collision mask 1 and anything they can hide behind on collision layer 6
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	ray_cast_3d.global_position = player.global_position
+	ray_cast_3d.global_position.z = 5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -27,4 +30,5 @@ func _process(delta):
 	
 	if ray_cast_3d.is_colliding():
 		print(str(ray_cast_3d.get_collider()))
+		timer
 	
