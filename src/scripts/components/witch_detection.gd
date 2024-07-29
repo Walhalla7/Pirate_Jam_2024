1f7377f0c91c6c5ceaa3a5de336cd14d087a9f75
Kyle Jardine <kjardine00@gmail.com>
2024-07-29 15:22:16 -0400

    Hand interpolation works need to work on the trigger for it on timeout

--- src/scripts/components/witch_detection.gd
+++ src/scripts/components/witch_detection.gd
@@ -8,27 +8,50 @@ class_name WitchDetector
 
 @onready var ray_cast_3d = $RayCast3D
 @onready var timer = $Timer
+@onready var hand = $CanvasLayer/Sprite2D
+@onready var point_a = $CanvasLayer/PointA
+@onready var point_b = $CanvasLayer/PointB
 
-signal caught
+var caught = false
+var t = 0.0
 
 # TO USE: Set the player to collision mask 1 and anything they can hide behind on collision layer 6
+# Connect assign player in the inspector
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ray_cast_3d.global_position = player.global_position
 	ray_cast_3d.global_position.z = 5
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	# Foll
+	# Follow the player left and right
 	ray_cast_3d.global_position.x = player.global_position.x
 	# Follow the player up and down
 	ray_cast_3d.global_position.y = player.global_position.y
 	
-	
 	if ray_cast_3d.is_colliding():
-		print(str(ray_cast_3d.get_collider()))
-		timer
+		#print(str(ray_cast_3d.get_collider()))
+		
+		if ray_cast_3d.get_collider() == player:
+			timer.start()
+			
+			print("start")
+			print(str(timer.time_left))
+		elif ray_cast_3d.get_collider() != player || ray_cast_3d.is_colliding() == false:
+			timer.stop()
+			print("stop")
+			
+	if caught:
+		print("caught")
+		move_hand(delta)
+		caught = false
+
+
+func move_hand(delta):
+	t += delta * 0.4
+	hand.position = point_a.position.lerp(point_b.position, t)
 	
+
+func _on_timer_timeout():
+	caught = true
