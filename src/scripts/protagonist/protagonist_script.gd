26ec71da7b302e6f0f8e6e9e7b13746465c9fe97
Sparta-900 <175566987+Sparta-900@users.noreply.github.com>
2024-07-30 18:14:10 -0500

    sound adjust and jump sound

--- src/scripts/protagonist/protagonist_script.gd
+++ src/scripts/protagonist/protagonist_script.gd
@@ -21,6 +21,8 @@ func _check_is_grounded():
 @onready var leftDetector = $Raycasts/Left_detector
 @onready var backDetector = $Raycasts/Back_detector
 @onready var jump = $jump
+
+
 #======================================== 	Variables 	==================================
 #Movement variables
 @export var move_speed = 5.0
@@ -89,7 +91,6 @@ func wall_jump():
 	can_crawl = false
 	climbTimer.stop()
 	velocity = wall_jump_velocity
-	jump.play()
 
 #function to calulcate directional inputs and movements 
 func _handle_move_input():
