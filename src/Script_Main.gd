9c7ac8c68cef549c10bf32e11f5c0d54f7969d7e
a-rat-in-a-hat <157244986+a-rat-in-a-hat@users.noreply.github.com>
2024-07-30 22:34:31 -0500

    end scene start

--- src/Script_Main.gd
+++ src/Script_Main.gd
@@ -2,10 +2,18 @@ extends Node
 
 @onready var dustbunnies = [$Interactable/DustBunny1, $Interactable/DustBunny2, $Interactable/DustBunny3, $Interactable/DustBunny4]
 var time :float
-
+@onready var cauldron_poisoned = false
 
 #variable declarations for intro
 @onready var CauldronCamera = $CauldronCamera
+
+
+#outro variables (the code will be bad deal with it)
+@onready var cauldron_camera = $CauldronCamera
+var transition_speed :float
+var position_difference :float
+
+
 
 #======================================== 	Initialize 	==================================
 func _ready():
@@ -14,7 +22,15 @@ func _ready():
 #======================================== 	Process 	==================================
 func _physics_process(delta):
 	time += delta
-	
+	if cauldron_poisoned:
+		transition_speed += delta/100
+		cauldron_camera.transform = cauldron_camera.transform.interpolate_with($CauldronCamera2.transform, transition_speed)
+		#interpolate cam to cauldron
+		
+		#positional diff check
+		#color change + bubble color change
+		#signal emit: SignalBus.emit_signal("Victory")
+		pass
 	animate_dust_bunnies(delta)
 
 #moves the dustbunny sprites in space
@@ -31,4 +47,4 @@ func _on_cauldron_zone_body_entered(body):
 		SignalBus.emit_signal("game_over")
 		
 	if body.is_in_group("Poison"):
-		SignalBus.emit_signal("Victory")
+		cauldron_poisoned = true
